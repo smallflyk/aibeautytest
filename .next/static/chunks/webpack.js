4029c019f9de89342ad48e490e8624f82145b497
smallflyk <GitHub>
2025-05-10 23:42:46 +0800

    添加SEO优化：favicon配置、robots标签、sitemap及其他缺失元素

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ee537d06d793f0c3"; }
+/******/ 		__webpack_require__.h = function() { return "572917de30e6c43a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
